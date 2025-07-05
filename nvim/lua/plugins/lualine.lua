3597405926625bd8fdcab9f807107f3e973a411e
Paulo Roberto Urio <paulo.urio@c6bank.com>
2025-07-05 07:20:53 -0300

    feat: setup lualine
    Signed-off-by: Paulo Roberto Urio <paulo.urio@c6bank.com>

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,9 +1,37 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("lualine").setup({
-      options = { theme = "powerline_dark" } 
-    })
-  end,
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "tokyonight",
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff" },
+				lualine_c = {
+					{
+						"filename",
+						path = 1,
+						symbols = {
+							modified = "[+]",
+							readonly = "[-]",
+							unnamed = "[No Name]",
+							newfile = "[New]",
+						},
+					},
+					{
+						"diagnostics",
+					},
+				},
+				lualine_x = {
+					"encoding",
+					"fileformat",
+					"filetype",
+				},
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+		})
+	end,
 }
